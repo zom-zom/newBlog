fc80986e7b540b6bd1b2a9d30b143876330895b8
Ty Zhou <TyZhou0112@outlook.com>
2025-03-13 02:18:27 +0800

    修改site

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "bradomcol.me",
+  site: "https://zom-zom.github.io",
   base: "newblog",
   trailingSlash: "always",
   integrations: [
