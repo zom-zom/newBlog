c9be180939e786e228af56019aa33a49ef5facbc
Ty Zhou <TyZhou0112@outlook.com>
2025-03-13 01:28:02 +0800

    修改域名

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://bradomcol.me/",
-  /*base: "/",*/
+  site: "https://bradomcol.me",
+  //base: "/",
   trailingSlash: "always",
   integrations: [
     tailwind(
