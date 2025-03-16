9eefa42785e9ea1cba22c972458f93a9a05cb60b
Ty Zhou <TyZhou0112@outlook.com>
2025-03-17 00:31:56 +0800

    更新about

--- src/config.ts
+++ src/config.ts
@@ -11,8 +11,8 @@ export const siteConfig: SiteConfig = {
   subtitle: 'bradom',
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: true,     // Hide the theme color picker for visitors
+    hue: 290,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
     enable: true,
@@ -26,7 +26,7 @@ export const siteConfig: SiteConfig = {
   },
   toc: {
     enable: true,           // Display the table of contents on the right side of the post
-    depth: 2                // Maximum heading depth to show in the table, from 1 to 3
+    depth: 3                // Maximum heading depth to show in the table, from 1 to 3
   },
   favicon: [    // Leave this array empty to use the default favicon
     // {
